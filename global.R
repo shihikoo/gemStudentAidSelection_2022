486be62572ec1abb0caca58110d5e009ac3daa28
Jing <shihikoo@gmail.com>
2023-06-29 20:04:43 -0400

    update

--- global.R
+++ global.R
@@ -20,10 +20,6 @@ gs4_deauth()
 
 df2022 <- clean_student_df2022(googlesheets4::read_sheet(googleSheetId2022, sheet = "studinfo_GEM_20220410"))
 
-df2023_submission <- clean_student_df2022(googlesheets4::read_sheet(googleSheetId2023, sheet = "submission"))
-df2023_recommendation <- clean_student_df2022(googlesheets4::read_sheet(googleSheetId2023, sheet = "recommendation"))
-df2023_tutorial <- clean_student_df2022(googlesheets4::read_sheet(googleSheetId2023, sheet = "tutorial"))
-df2023_rep <- clean_student_df2022(googlesheets4::read_sheet(googleSheetId2023, sheet = "student rep"))
 
 
 
